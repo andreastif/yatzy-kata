e2d7283337dcc4b214f42218bcd759ca4d143f16
andreastif <45339645+andreastif@users.noreply.github.com>
2023-01-16 12:10:17 +0100

    TDD per malmen

--- src/main/java/com/thehiflyer/kata/yatzy/Category.java
+++ src/main/java/com/thehiflyer/kata/yatzy/Category.java
@@ -3,4 +3,16 @@ package com.thehiflyer.kata.yatzy;
 public enum Category {
 	CHANCE
 	// add more categories here as you implement them
+
+	private final int value;
+
+	Category(final int newValue) {
+		value = newValue;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
 }
+
--- src/test/java/com/thehiflyer/kata/yatzy/YatzyTest.java
+++ src/test/java/com/thehiflyer/kata/yatzy/YatzyTest.java
@@ -3,6 +3,7 @@ package com.thehiflyer.kata.yatzy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class YatzyTest {
 
